b2c8086bf3821da2968dc644c3ace3f5e0dbac1f
Agus y Jime <Agus y Jime@DESKTOP-KCVTS1S>
2022-09-24 17:27:07 -0300

    Agregar archivos de proyecto.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 0;
+            Console.WriteLine("Ingrese un número:");
+            int k = int.Parse(Console.ReadLine());
+            while (i < k)
+            {
+                Console.WriteLine(i);
+                int l = i + j;
+                j = i;
+                i = l;
+            }
+        }
+    }
+}
